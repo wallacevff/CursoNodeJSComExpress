a2490f7e86dbf0d9e1aed4ef1df982ccc2011e3e
Wallace Vidal de Figueiredo Fortuna <wallacevff@hotmail.com>
2021-12-12 21:38:39 -0300

    Inserindo Dados

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,12 @@
 // app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
-
+const Atendimento = require('../models/atendimentos')
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
 
     app.post('/atendimentos', (req, res) => {
-        console.log(req.body);
+       const atendimento = req.body
+       Atendimento.adiciona(atendimento)
+        //console.log(req.body);
         res.send('Você está na rota de atendimentos e está realizando um POST')
     
     })
